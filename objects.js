90e93940e8752169c208c56f8ec1592c835d73b2
yash desai <yhdesai99@ymail.com>
2019-08-24 13:38:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-Object.assign({}, playlist, { artistName: null })
+delete playlist[artistName]
 return playlist
 }
