1bf6a9d2e6b17cbc9a697fcdfc69a72f1d30fe38
yash desai <yhdesai99@ymail.com>
2019-08-24 13:34:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, { artistName: songTitle })
 }
+
+function removeFromPlaylist(playlist, artistName) {
+delete playlist.artistName
+return playlist  
+}
